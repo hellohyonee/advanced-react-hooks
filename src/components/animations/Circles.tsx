adb3c5051153a0ff22cfb22c8d0d75badd4fc360
hellohyonee <hellohk07@gmail.com>
2022-07-15 16:20:49 +0900

    Circle 컴포넌트 재사용하여 Circle2 만들기.

--- src/components/animations/Circles.tsx
+++ src/components/animations/Circles.tsx
@@ -5,6 +5,7 @@ function Circles() {
   return (
     <Wrapper>
       <Circle />
+      <Circle2 />
     </Wrapper>
   )
 }
@@ -29,4 +30,11 @@ const Circle = styled.div`
   border-radius: 210px;
   left: calc(50% - 420px / 2);
   top: calc(50% - 420px / 2);
+`
+const Circle2 = styled(Circle)`
+  width: 260px;
+  height: 260px;
+  border-radius: 135px;
+  left: calc(50% - 260px / 2);
+  top: calc(50% - 260px / 2);
 `
